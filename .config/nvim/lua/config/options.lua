b5be110a3e8d7f34114b527a9a0bcde877debe60
Andrea Zerbini <zerbini.andrea@proton.me>
2025-06-29 22:57:15 +0200

    add indentation to nvim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,4 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+vim.opt.tabstop = 4 -- Width of tab character
+vim.opt.shiftwidth = 4 -- Width of indentation
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.smartindent = true
