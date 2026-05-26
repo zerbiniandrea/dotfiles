ce61595624ca72640abf0bae017339e62868533f
zerby <zerby.programming@proton.me>
2026-05-26 16:03:36 +0200

    replace diffview with codediff

--- .config/nvim/lua/plugins/git-integrations.lua
+++ .config/nvim/lua/plugins/git-integrations.lua
@@ -17,82 +17,30 @@ return {
     'tpope/vim-fugitive',
   },
   {
-    'sindrets/diffview.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      -- Workaround for noice.nvim E445 error on close
-      local function diffview_safe_close()
-        local hidden = vim.opt.hidden:get()
-        vim.opt.hidden = true
-        vim.cmd('DiffviewClose')
-        vim.opt.hidden = hidden
-      end
-
-      require('diffview').setup {
-        enhanced_diff_hl = true,
+    'esmuellert/codediff.nvim',
+    cmd = 'CodeDiff',
+    opts = {
+      diff = {
+        layout = 'side-by-side',
+      },
+      explorer = {
+        position = 'left',
+        width = 40,
+        view_mode = 'tree',
+        flatten_dirs = true,
+      },
+      keymaps = {
         view = {
-          default = {
-            layout = 'diff2_horizontal',
-            winbar_info = true,
-            disable_diagnostics = true,
-          },
-          merge_tool = {
-            layout = 'diff3_horizontal',
-            disable_diagnostics = true,
-            winbar_info = true,
-          },
-          file_history = {
-            layout = 'diff2_horizontal',
-            winbar_info = true,
-            disable_diagnostics = true,
-          },
+          next_file = '<Tab>',
+          prev_file = '<S-Tab>',
         },
-        file_panel = {
-          listing_style = 'tree',
-          tree_options = {
-            flatten_dirs = true,
-            folder_statuses = 'only_folded',
-          },
-          win_config = {
-            position = 'left',
-            width = 40,
-          },
-          indent = 1,
-        },
-        file_history_panel = {
-          win_config = {
-            position = 'bottom',
-            height = 16,
-          },
-        },
-        signs = {
-          fold_closed = '',
-          fold_open = '',
-          done = '✓',
-        },
-        icons = {
-          folder_closed = '',
-          folder_open = '',
-        },
-        hooks = {},
-        keymaps = {
-          view = {
-            { 'n', 'q', diffview_safe_close, { desc = 'Close diffview' } },
-          },
-          file_panel = {
-            { 'n', 'q', diffview_safe_close, { desc = 'Close diffview' } },
-          },
-          file_history_panel = {
-            { 'n', 'q', diffview_safe_close, { desc = 'Close diffview' } },
-          },
-        },
-      }
-
-      vim.keymap.set('n', '<leader>gd', '<cmd>DiffviewOpen<CR>', { desc = '[G]it [D]iff view' })
-      vim.keymap.set('n', '<leader>gD', diffview_safe_close, { desc = '[G]it [D]iff close' })
-      vim.keymap.set('n', '<leader>gh', '<cmd>DiffviewFileHistory<CR>', { desc = '[G]it [H]istory' })
-      vim.keymap.set('n', '<leader>gH', '<cmd>DiffviewFileHistory %<CR>', { desc = '[G]it [H]istory (current file)' })
-    end,
+      },
+    },
+    keys = {
+      { '<leader>gd', '<cmd>CodeDiff<CR>', desc = '[G]it [D]iff view' },
+      { '<leader>gh', '<cmd>CodeDiff history<CR>', desc = '[G]it [H]istory' },
+      { '<leader>gH', '<cmd>CodeDiff history HEAD %<CR>', desc = '[G]it [H]istory (current file)' },
+    },
   },
   {
     'akinsho/git-conflict.nvim',
